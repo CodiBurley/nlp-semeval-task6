c3f910ea79754cf50a258e6fa85e1b61ceeaad0b
Daniel Wendelken <wendeldr@mail.uc.edu>
2017-11-26 18:05:17 -0500

    changed custfunctions

--- subtask-b/lib/custFunctions.py
+++ subtask-b/lib/custFunctions.py
@@ -13,39 +13,38 @@ def HashtagToken(sent):
     returnTweet = [w for w in tweet if '#' in w]
     return returnTweet
 
+def atToken(sent):
+    tweet = TweetTokenizer().tokenize(sent)
+    returnTweet = [w for w in tweet if '@' in w]
+    return returnTweet
+
 def tfidfVector(tweets):
     vectorizer = TfidfVectorizer(stop_words='english')
     return vectorizer.fit_transform(tweets)
 
 def HashtagVector(tweets):
-    ngram_vectorizer = CountVectorizer(ngram_range=(1, 1), analyzer='word', stop_words='english', min_df=10, tokenizer=lambda x: HashtagToken(x))
-    return ngram_vectorizer.fit_transform(tweets)
+    hashtag_vectorier = TfidfVectorizer(ngram_range=(1, 1), analyzer='word', lowercase=True,stop_words='english', min_df=10, tokenizer=lambda x: HashtagToken(x))
+    return hashtag_vectorier.fit_transform(tweets)
 
+def atVector(tweets):
+    hashtag_vectorier = TfidfVectorizer(ngram_range=(1, 1), analyzer='word', lowercase=True,stop_words='english', min_df=10, tokenizer=lambda x: atToken(x))
+    return hashtag_vectorier.fit_transform(tweets)
 
+def posToken(sent):
+    token = nltk.pos_tag(TweetTokenizer().tokenize(sent))
+    adj = []
+    for w in token:
+        if 'JJ' in w[1]:
+            adj.append(w[0])
+    return(adj)
+     
 def adjVector(tweets):
     stops = set(stopwords.words("english"))
     tweets = tweets.apply(lambda x: x.lower())
     tweets = tweets.apply(lambda x: ' '.join([word for word in TweetTokenizer().tokenize(x) if 'http' not in word and 'https' not in word]))
     tweets = tweets.apply(lambda x: ' '.join([word for word in TweetTokenizer().tokenize(x) if '/' not in word]))
-    tweets = tweets.apply(lambda x: ' '.join([word for word in TweetTokenizer().tokenize(x) if '\\x' not in word]))
     tweets = tweets.apply(lambda x: ' '.join([word for word in TweetTokenizer().tokenize(x) if word not in stops and '@' not in word]))
     tweets = tweets.apply(lambda x: ' '.join([word for word in TweetTokenizer().tokenize(x) if word not in stops and '#' not in word]))
-    pos = map(lambda s: nltk.pos_tag(TweetTokenizer().tokenize(s)), tweets)
-    pos = list(pos)
-
-    adj = []
-    for t in pos:
-        for w in t:
-            if 'JJ' in w[1]:
-                adj.append(w[0])
-                
-    adj = sorted(set(adj))
-
-    fit = []
-    for tweet in tweets:
-        ohv = [0] * len(adj)
-        for word in TweetTokenizer().tokenize(tweet):
-            if word in adj:
-                ohv[adj.index(word)] += 1
-        fit.append(ohv)
-    return np.array(fit)
+    
+    pos_vectorier = TfidfVectorizer(ngram_range=(1, 1), analyzer='word', lowercase=True,stop_words='english', min_df=10, tokenizer=lambda x: posToken(x))
+    return pos_vectorier.fit_transform(tweets)
